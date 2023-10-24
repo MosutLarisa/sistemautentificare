e376b3b3a371e88cb92871cc5441bf8eb280f101
MosutLarisa <larisa.mosut@emanuel.ro>
2023-10-24 17:32:49 +0300

    sistemautentificare

---
+++ sistemautentificare/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace sistemautentificare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string usernameCorect = "Larisa";
+            const string parolaCorecta = "Be Kind";
+
+            bool autentificat = false;
+
+            while (!autentificat)
+            {
+                Console.WriteLine("Introduceti username: ");
+                string username = Console.ReadLine();
+
+                Console.WriteLine("Introduceti parola: ");
+                string parola = Console.ReadLine();
+
+                if (username == usernameCorect && parola == parolaCorecta)
+                {
+                    Console.WriteLine("Bine ai venit, " + usernameCorect);
+                    autentificat = true;
+                }
+                else
+
+                {
+                    Console.WriteLine("Autentificare esuata. Introduceti din nou.");
+                }
+
+                
+            }
+                
+        }
+    }
+}
